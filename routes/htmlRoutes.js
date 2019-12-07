308d66fe9a3d19c9bfd21946bbdc9d24e3eb29bc
Alex Ogilvie <ogilvie1231@gmail.com>
2019-12-07 12:44:33 -0700

    fixing html routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -11,12 +11,25 @@ module.exports = function(app) {
     });
   });
 
-  // app.get("/home", function(req, res) {
-  //   res.render("home", {
-  //     msg: "Welcome!",
-  //     examples: dbExamples
+  app.get("/listing", function(req, res) {
+    res.render("listing");
+  });
+
+  app.get("/contact", function(req, res) {
+    res.render("contact");
+  });
+
+  app.get("/post", function(req, res) {
+    res.render("post");
+  });
+
+  // app.get("/listing", function(req, res) {
+  //      res.render("listing", {
+  //      msg: "Welcome!",
+  //      examples: dbExamples
   //   });
   // });
+
 
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
