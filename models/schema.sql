09106875541e3d231c40406371a2712af0636077
benklinton <benklinton@gmail.com>
2019-12-07 10:15:04 -0700

    tweaked schema

--- models/schema.sql
+++ models/schema.sql
@@ -9,7 +9,7 @@ CREATE TABLE products (
     product_name VARCHAR(100) NULL,
     product_category VARCHAR(42) NULL,
     product_description VARCHAR(1000) NULL,
-    product_image VARCHAR (21) NULL,
+    product_image_link VARCHAR (500) NULL,
     price DECIMAL (6,2),
     quanity INT(10),
     PRIMARY KEY (id)
