691e4f7b1bc6ed5773185119daa7b6effb576f05
benklinton <benklinton@gmail.com>
2019-12-07 11:46:03 -0700

    updated sequelize files

--- models/products.js
+++ models/products.js
@@ -1,7 +1,11 @@
 module.exports = function(sequelize, DataTypes) {
   var Products = sequelize.define("Products", {
-    text: DataTypes.STRING,
-    description: DataTypes.TEXT
+    product_name: DataTypes.STRING,
+    product_catagory: DataTypes.STRING,
+    product_description: DataTypes.TEXT,
+    product_image_link: DataTypes.TEXT,
+    price: DataTypes.FLOAT,
+    quantity: DataTypes.INTEGER
   });
   return Products;
 };
