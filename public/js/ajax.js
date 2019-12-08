f30ae435462061ce8f9cc5a0a175c771af211e2c
benklinton <benklinton@gmail.com>
2019-12-07 21:08:18 -0700

    Fixed AJAX call

--- public/js/ajax.js
+++ public/js/ajax.js
@@ -1,10 +1,20 @@
-$("#product_name").val().trim();
-$("#product_catagory").val().trim();
-$("#product_description").val().trim();
-$("#price").val().trim();
-$("#quanity").val().trim();
+$("#submit").click(function () {
 
+    var product_name = $("#product_name").val().trim();
+    var product_catagory = $("#product_catagory").val().trim();
+    var product_description = $("#product_description").val().trim();
+    var price = $("#price").val().trim();
+    var quantiy = $("#quanity").val().trim();
 
-$.ajax(
-    
-).then()
+    $.post("/api/post",
+        {
+            product_name: product_name,
+            product_catagory: product_catagory,
+            product_description: product_description,
+            price: price,
+            quantiy: quantiy
+        },
+        function () {
+            console.log("Success!")
+        });
+});
