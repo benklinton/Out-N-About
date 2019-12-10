bdd9ece13f8874422c019b091fe24042d36610b3
benklinton <benklinton@gmail.com>
2019-12-09 17:04:41 -0700

    fixed minor spelling bugs

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -12,7 +12,7 @@ module.exports = function(app) {
   app.post("/api/sell", function(req, res) {
     db.Products.create({
       product_name: req.body.product_name,
-      product_catagory: req.body.product_catagory,
+      product_category: req.body.product_category,
       product_description: req.body.product_description,
       product_image_link: req.body.product_image_link,
       price: req.body.price,
